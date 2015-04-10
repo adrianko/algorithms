26c014a8055700dfffa44bbc4f16e0283852aeb4
Adrian Ko <adrianchko@gmail.com>
2015-04-10 11:38:39 +0100

    add a BubbleSort test

--- src/test/sorting/BubbleSortTest.java
+++ src/test/sorting/BubbleSortTest.java
@@ -1,5 +1,24 @@
 package test.sorting;
 
+import main.sorting.BubbleSort;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class BubbleSortTest {
+    
+    @Test
+    public void testSortInteger() {
+        List<Integer> data = new LinkedList<>(Arrays.asList(2, 6, 1, 5, 3, 9, 7));
+        List<Integer> exp = new LinkedList<>(Arrays.asList(1, 2, 3, 5, 6, 7, 9));
+        BubbleSort bubble = new BubbleSort(data);
+        bubble.sort();
+        List<Integer> act = bubble.getData();
+
+        Assert.assertEquals(exp, act);
+    }
     
 }
