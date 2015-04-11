792a7898b2c6eeb1dbcc2f32deb527f331c5816d
AK <adrianchko@gmail.com>
2015-04-11 18:33:50 +0100

    Add QuickSortTest

---
+++ src/test/sorting/QuickSortTest.java
@@ -0,0 +1,23 @@
+package test.sorting;
+
+import main.sorting.QuickSort;
+import org.junit.Assert;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class QuickSortTest {
+
+    public void testSortInteger() {
+        List<Integer> data = new LinkedList<>(Arrays.asList(2, 6, 1, 5, 3, 9, 7));
+        List<Integer> exp = new LinkedList<>(Arrays.asList(1, 2, 3, 5, 6, 7, 9));
+        QuickSort quick = new QuickSort(data);
+        quick.sort();
+        List<Integer> act = quick.getData().stream().collect(Collectors.toList());
+
+        Assert.assertEquals(exp, act);
+    }
+
+}
