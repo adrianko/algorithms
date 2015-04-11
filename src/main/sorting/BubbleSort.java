72b3fa8a7f5d1c63fe91023a5bfcb5083caa927c
AK <adrianchko@gmail.com>
2015-04-11 12:55:50 +0100

    Box calculation

--- src/main/sorting/BubbleSort.java
+++ src/main/sorting/BubbleSort.java
@@ -25,7 +25,7 @@ public class BubbleSort {
         ArrayList<Integer> arr = new ArrayList<>(getData());
         
         for (int i = 0; i < (getData().size() - 1); i++) {
-            for (int j = 0; j < getData().size() - i - 1; j++) {
+            for (int j = 0; j < (getData().size() - i - 1); j++) {
                 if (arr.get(j) > arr.get(j + 1)) {
                     Collections.swap(arr, j, j + 1);
                 }
