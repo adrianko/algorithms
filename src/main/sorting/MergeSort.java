f427e0f8e71b86a14a19732cbbd24112ac2308e2
AK <adrianchko@gmail.com>
2015-04-12 16:23:43 +0100

    Use mergesort and merge to sort

--- src/main/sorting/MergeSort.java
+++ src/main/sorting/MergeSort.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 public class MergeSort extends AbstractSort {
 
     private ArrayList<Integer> arr;
+    private ArrayList<Integer> helper;
 
     public MergeSort(Collection<Integer> data) {
         super(data);
@@ -14,7 +15,9 @@ public class MergeSort extends AbstractSort {
     @Override
     public void sort() {
         arr = new ArrayList<>(getData());
+        helper = new ArrayList<>();
         mergesort(0, arr.size() - 1);
+        data = arr;
     }
 
     private void mergesort(int low, int high) {
@@ -27,7 +30,31 @@ public class MergeSort extends AbstractSort {
     }
 
     private void merge(int low, int middle, int high) {
+        for (int i = low; i <= high; i++) {
+            helper.add(i, arr.get(i));
+        }
 
+        int i = low;
+        int j = middle + 1;
+        int k = low;
+
+        while (i <= middle && j <= high) {
+            if (helper.get(i) <= helper.get(j)) {
+                arr.set(k, helper.get(i));
+                i++;
+            } else {
+                arr.set(k, helper.get(j));
+                j++;
+            }
+
+            k++;
+        }
+
+        while (i <= middle) {
+            arr.set(k, helper.get(i));
+            k++;
+            i++;
+        }
     }
 
 }
