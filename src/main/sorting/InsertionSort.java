4d2a524798990492892df74e62f75ec1f3ed67be
Adrian Ko <adrianchko@gmail.com>
2015-04-13 09:49:13 +0100

    Insertion sort explanation

--- src/main/sorting/InsertionSort.java
+++ src/main/sorting/InsertionSort.java
@@ -3,6 +3,12 @@ package main.sorting;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+ * Insertion Sort - O(n^2) - Splits array into two sub-arrays. The first sub-array is always sorted and increases in
+ * size as the sort continues. The second sub-array is unsorted, contains all the elements yet to be inserted into the
+ * first sub-array, and decreases in size as the sort continues. With each pass through the loop, the next element in
+ * the second sub-array is placed into its proper position in the first sorted sub-array.
+ */
 public class InsertionSort extends AbstractSort {
     
     public InsertionSort(Collection<Integer> data) {
