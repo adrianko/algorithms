314e2f910b4288f5bd42260a1eeba54a5323ca7e
Adrian Ko <adrianchko@gmail.com>
2015-04-13 09:38:57 +0100

    Add insertion sort algorithm

--- src/main/sorting/InsertionSort.java
+++ src/main/sorting/InsertionSort.java
@@ -1,5 +1,6 @@
 package main.sorting;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class InsertionSort extends AbstractSort {
@@ -10,7 +11,20 @@ public class InsertionSort extends AbstractSort {
     
     @Override
     public void sort() {
+        ArrayList<Integer> arr = new ArrayList<>(getData());
         
+        for (int i = 1; i < arr.size(); i++) {
+            int temp = arr.get(i);
+            int j;
+            
+            for (j = (i - 1); j >= 0 && temp < arr.get(j); j--) {
+                arr.set(j + 1, arr.get(j));
+            }
+            
+            arr.set(j + 1, temp);
+        }
+        
+        super.setData(arr);
     }
     
 }
