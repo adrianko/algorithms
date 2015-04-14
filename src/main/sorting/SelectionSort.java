6119cfa89890983fde20388f939726a8f7a24b36
Adrian Ko <adrianchko@gmail.com>
2015-04-14 09:26:05 +0100

    Add Selection sort algorithm

--- src/main/sorting/SelectionSort.java
+++ src/main/sorting/SelectionSort.java
@@ -1,5 +1,6 @@
 package main.sorting;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class SelectionSort extends AbstractSort {
@@ -10,7 +11,23 @@ public class SelectionSort extends AbstractSort {
     
     @Override
     public void sort() {
+        ArrayList<Integer> arr = new ArrayList<>(getData());
         
+        for (int i = 0; i < arr.size(); i++) {
+            int index = i;
+            
+            for (int j = i + 1; j < arr.size(); j++) {
+                if (arr.get(j) < arr.get(index)) {
+                    index = j;
+                }
+            }
+            
+            int smaller = arr.get(index);
+            arr.set(index, arr.get(i));
+            arr.set(i, smaller);
+        }
+        
+        setData(arr);
     }
     
 }
