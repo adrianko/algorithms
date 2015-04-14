7707a0b9b1475016bce959268f94909849f76713
Adrian Ko <adrianchko@gmail.com>
2015-04-14 09:32:18 +0100

    Add selection sort explanation

--- src/main/sorting/SelectionSort.java
+++ src/main/sorting/SelectionSort.java
@@ -3,6 +3,11 @@ package main.sorting;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+ * Selection Sort - O(n^2) - Array is divided into nominal sorted and unsorted sublists. At every step, the algorithm 
+ * finds the smallest element in the unsorted sublist and adds it to the end of the sorted sublist. When the unsorted 
+ * sublist becomes empty, sorting is complete.
+ */
 public class SelectionSort extends AbstractSort {
     
     public SelectionSort(Collection<Integer> data) {
