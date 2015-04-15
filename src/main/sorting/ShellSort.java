87e59b65c806ca4ea7c602cc879b57fd843df308
Adrian Ko <adrianchko@gmail.com>
2015-04-15 09:42:21 +0100

    Add shell sort exp

--- src/main/sorting/ShellSort.java
+++ src/main/sorting/ShellSort.java
@@ -3,6 +3,13 @@ package main.sorting;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+ * Shell Sort - O(n log^2 n) - Generalisation of insertion sort that allows the exchange of items that are far apart.
+ * The original list is broken into a smaller number of sublists, each of which is sorted using an insertion sort. The 
+ * unique way that these sublists are chosen is the key to the shell sort. Instead of breaking the list into sublists of
+ * contiguous items, the shell sort uses an increment 'i' called the gap to create a sublist be choosing items that are 
+ * 'i' numbers apart.
+ */
 public class ShellSort extends AbstractSort {
     
     public ShellSort(Collection<Integer> data) {
