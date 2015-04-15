443f0617eec6f12dbd9ebe303ababa3f0d2c5c6d
Adrian Ko <adrianchko@gmail.com>
2015-04-15 09:34:30 +0100

    Add ShellSort alg

--- src/main/sorting/ShellSort.java
+++ src/main/sorting/ShellSort.java
@@ -1,5 +1,6 @@
 package main.sorting;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class ShellSort extends AbstractSort {
@@ -10,7 +11,30 @@ public class ShellSort extends AbstractSort {
     
     @Override
     public void sort() {
+        ArrayList<Integer> arr = new ArrayList<>(getData());
+        int incr = arr.size() / 2;
         
+        while (incr > 0) {
+            for (int i = incr; i < arr.size(); i++) {
+                int j = i;
+                int temp = arr.get(i);
+                
+                while (j >= incr && arr.get(j - incr) > temp) {
+                    arr.set(j, arr.get(j - incr));
+                    j = j - incr;
+                }
+                
+                arr.set(j, temp);
+            }
+            
+            if (incr == 2) {
+                incr = 1;
+            } else {
+                incr *= (5.0 / 11);
+            }
+        }
+        
+        setData(arr);
     }
     
 }
