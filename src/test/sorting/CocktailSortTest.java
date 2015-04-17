a3e84c5db184c59c4a1b9f63576c879b63184666
Adrian Ko <adrianchko@gmail.com>
2015-04-17 09:23:12 +0100

    Add CockTailSort.java

---
+++ src/test/sorting/CocktailSortTest.java
@@ -0,0 +1,22 @@
+package test.sorting;
+
+import main.sorting.CocktailSort;
+import main.sorting.Sort;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CocktailSortTest extends SortTest {
+
+    @Test
+    public void testSortInteger() {
+        Sort cocktail = new CocktailSort(getData());
+        cocktail.sort();
+        List<Integer> act = cocaktail.getData().stream().collect(Collectors.toList());
+
+        Assert.assertEquals(getExp(), act);
+    }
+
+}
