06b0ca028fee6ca7e1607cd9b7eef87c83b5c290
Adrian Ko <adrianchko@gmail.com>
2015-04-18 16:20:53 +0100

    Create CombSort.java

---
+++ src/main/sorting/CombSort.java
@@ -0,0 +1,16 @@
+package main.sorting;
+
+import java.util.Collection;
+
+public class CombSort extends AbstractSort {
+    
+    public CombSort(Collection<Integer> data) {
+        super(data);
+    }
+    
+    @Override
+    public void sort() {
+        
+    }
+    
+}
