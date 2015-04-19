81b606686e55a86e87e1f9092d394d8d94540fbe
Adrian Ko <adrianchko@gmail.com>
2015-04-19 14:50:29 +0100

    Add GnomeSort.java

---
+++ src/main/sorting/GnomeSort.java
@@ -0,0 +1,16 @@
+package main.sorting;
+
+import java.util.Collection;
+
+public class GnomeSort extends AbstractSort {
+    
+    public GnomeSort(Collection<Integer> data) {
+        super(data);
+    }
+    
+    @Override
+    public void sort() {
+        
+    }
+    
+}
