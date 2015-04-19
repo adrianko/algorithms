85ff22101fc5b67338b0c3e0c3f7a7e947eac67c
Adrian Ko <adrianchko@gmail.com>
2015-04-19 14:54:09 +0100

    GnomeSort alg

--- src/main/sorting/GnomeSort.java
+++ src/main/sorting/GnomeSort.java
@@ -1,6 +1,8 @@
 package main.sorting;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class GnomeSort extends AbstractSort {
     
@@ -10,7 +12,22 @@ public class GnomeSort extends AbstractSort {
     
     @Override
     public void sort() {
+        ArrayList<Integer> arr = new ArrayList<Integer>(getData());
         
+        int i = 1;
+        int j = 2;
+        
+        while (i < arr.size()) {
+            if (arr.get(i - 1) <= arr.get(i)) {
+                i = j;
+                j++;
+            } else {
+                Collections.swap(arr, i - 1, i--);
+                i = (i == 0) ? j++ : i;
+            }
+        }
+        
+        setData(arr);
     }
     
 }
