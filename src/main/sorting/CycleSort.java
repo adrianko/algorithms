f93c3a24959dd4b5cdb8d567c6be224c5e37fef7
Adrian Ko <adrianchko@gmail.com>
2015-04-20 11:04:06 +0100

    Add CycleSort.java

---
+++ src/main/sorting/CycleSort.java
@@ -0,0 +1,16 @@
+package main.sorting;
+
+import java.util.Collection;
+
+public class CycleSort extends AbstractSort {
+    
+    public CycleSort(Collection<Integer> data) {
+        super(data);    
+    }
+    
+    @Override
+    public void sort() {
+        
+    }
+    
+}
