be71217ccc9b1d4d7f9c1d97e0732c060f74271f
Adrian Ko <adrianchko@gmail.com>
2015-04-20 11:09:44 +0100

    Add cycle sort

--- src/main/sorting/CycleSort.java
+++ src/main/sorting/CycleSort.java
@@ -1,5 +1,6 @@
 package main.sorting;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class CycleSort extends AbstractSort {
@@ -10,7 +11,53 @@ public class CycleSort extends AbstractSort {
     
     @Override
     public void sort() {
+        ArrayList<Integer> arr = new ArrayList<>(getData());
+        int writes = 0;
         
+        for (int cycle = 0; cycle < arr.size() - 1; cycle++) {
+            int val = arr.get(cycle);
+            int pos = cycle;
+            
+            for (int i = cycle + 1; i < arr.size(); i++) {
+                if (arr.get(i) < val) {
+                    pos++;
+                }
+            }
+
+            if (pos == cycle) {
+                continue;
+            }
+
+            while (val == arr.get(pos)) {
+                pos++;
+            }
+
+            int temp = arr.get(pos);
+            arr.set(pos, val);
+            val = temp;
+            writes++;
+
+            while (pos != cycle) {
+                pos = cycle;
+
+                for (int i = cycle + 1; i < arr.size(); i++) {
+                    if (arr.get(i) < val) {
+                        pos++;
+                    }
+                }
+                
+                while (val == arr.get(pos)) {
+                    pos++;
+                }
+
+                temp = arr.get(pos);
+                arr.set(pos, val);
+                val = temp;
+                writes++;
+            }
+        }
+        
+        setData(arr);
     }
     
 }
