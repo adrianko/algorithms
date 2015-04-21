56f0abe037f3cf287729a2495ff38220f6bea09e
AK <adrianchko@gmail.com>
2015-04-21 18:50:41 +0100

    Permutation Sort exp

--- src/main/sorting/PermutationSort.java
+++ src/main/sorting/PermutationSort.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Permutation Sort - O(too long) - Generates all permutations until sorted one is found
+ */
 public class PermutationSort extends AbstractSort {
 
     public PermutationSort(Collection<Integer> data) {
