2ea29757db125c2ab952f8a8c6a7e5adb5df81f3
AK <adrianchko@gmail.com>
2015-04-21 16:48:59 +0100

    Add PermutationSort.java

---
+++ src/main/sorting/PermutationSort.java
@@ -0,0 +1,16 @@
+package main.sorting;
+
+import java.util.Collection;
+
+public class PermutationSort extends AbstractSort {
+
+    public PermutationSort(Collection<Integer> data) {
+        super(data);
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+}
