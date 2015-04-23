d9d153b1578895343de90c83900791233bd461e0
Adrian Ko <adrianchko@gmail.com>
2015-04-23 09:26:09 +0100

    Counting Sort imp

--- src/main/sorting/CountingSort.java
+++ src/main/sorting/CountingSort.java
@@ -1,6 +1,8 @@
 package main.sorting;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class CountingSort extends AbstractSort {
     
@@ -10,7 +12,26 @@ public class CountingSort extends AbstractSort {
     
     @Override
     public void sort() {
+        ArrayList<Integer> arr = new ArrayList<>(getData());
+        int max = Collections.max(arr);
+        int min = Collections.min(arr);
+        int[] count = new int[max - min + 1];
         
+        for (int n : arr) {
+            count[n - min]++;
+        }
+        
+        int z = 0;
+        
+        for (int i = min; i <= max; i++) {
+            while (count[i - min] > 0) {
+                arr.set(z, i);
+                z++;
+                count[i - min]--;
+            }
+        }
+        
+        setData(arr);
     }
     
 }
