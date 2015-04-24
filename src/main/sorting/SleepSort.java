36001e1d2a104a1af1e13fe370e18d4372859738
AK <adrianchko@gmail.com>
2015-04-24 10:24:22 +0100

    Add SleepSortTest.java

---
+++ src/main/sorting/SleepSort.java
@@ -0,0 +1,16 @@
+package main.sorting;
+
+import java.util.Collection;
+
+public class SleepSort extends AbstractSort {
+
+    public SleepSort(Collection<Integer> data) {
+        super(data);
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+}
