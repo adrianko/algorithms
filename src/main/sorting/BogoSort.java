bad89f9498bf7fab6300940a194fe1111c12d5c2
AK <adrianchko@gmail.com>
2015-04-25 12:10:35 +0100

    Finish BogoSort

--- src/main/sorting/BogoSort.java
+++ src/main/sorting/BogoSort.java
@@ -1,6 +1,9 @@
 package main.sorting;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Bogo Sort - O(n . n!) - Random shuffle and check if sorted.
@@ -13,7 +16,23 @@ public class BogoSort extends AbstractSort {
 
     @Override
     public void sort() {
+        ArrayList<Integer> arr = new ArrayList<>(getData());
 
+        while (!isSorted(arr)) {
+            Collections.shuffle(arr);
+        }
+
+        setData(arr);
+    }
+
+    private boolean isSorted(List<Integer> arr) {
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i - 1) > arr.get(i)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
