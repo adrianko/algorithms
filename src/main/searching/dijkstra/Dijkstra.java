cdfce7769bc699a3c7f1e518e1e5bd9481cec1c5
Adrian Ko <adrianchko@gmail.com>
2015-07-27 07:32:53 +0100

    Add vertices and edges

--- src/main/searching/dijkstra/Dijkstra.java
+++ src/main/searching/dijkstra/Dijkstra.java
@@ -42,4 +42,23 @@ public class Dijkstra {
         return path;
     }
     
+    public static void main(String[] args) {
+        Vertex v0 = new Vertex("London");
+        Vertex v1 = new Vertex("Birmingham");
+        Vertex v2 = new Vertex("Bristol");
+        Vertex v3 = new Vertex("Oxford");
+        Vertex v4 = new Vertex("Cambridge");
+        Vertex v5 = new Vertex("Southampton");
+        Vertex v6 = new Vertex("Portsmouth");
+
+        v0.adjacencies = new Edge[]{new Edge(v1, 79.83), new Edge(v5, 81.15)};
+        v1.adjacencies = new Edge[]{new Edge(v0, 79.75), new Edge(v2, 39.42), new Edge(v3, 103.00)};
+        v2.adjacencies = new Edge[]{new Edge(v1, 38.65)};
+        v3.adjacencies = new Edge[]{new Edge(v1, 102.53), new Edge(v5, 61.44), new Edge(v6, 96.79)};
+        v4.adjacencies = new Edge[]{new Edge(v5, 133.04)};
+        v5.adjacencies = new Edge[]{new Edge(v0, 81.77), new Edge(v3, 62.05), new Edge(v4,134.47), new Edge(v6, 91.63)};
+        v6.adjacencies = new Edge[]{new Edge(v3, 97.24), new Edge(v5, 87.94)};
+        Vertex[] vertices = { v0, v1, v2, v3, v4, v5, v6 };
+    }
+    
 }
