688dc3ffe974e8fc3035b29436ca7b6c5852efb8
Adrian Ko <adrianchko@gmail.com>
2015-07-27 07:24:54 +0100

    comparePaths for each vertex from a source

---
+++ src/main/searching/dijkstra/Dijkstra.java
@@ -0,0 +1,30 @@
+package main.searching.dijkstra;
+
+import java.util.PriorityQueue;
+
+public class Dijkstra {
+    
+    public static void computePaths(Vertex source) {
+        source.minDistance = 0;
+        PriorityQueue<Vertex> vertexQueue = new PriorityQueue<>();
+        vertexQueue.add(source);
+        
+        while (!vertexQueue.isEmpty()) {
+            Vertex u = vertexQueue.poll();
+            
+            for (Edge e : u.adjacencies) {
+                Vertex v = e.target;
+                double weight = e.weight;
+                double distanceThroughU = u.minDistance + weight;
+                
+                if (distanceThroughU < v.minDistance) {
+                    vertexQueue.remove(v);
+                    v.minDistance = distanceThroughU;
+                    v.previous = u;
+                    vertexQueue.add(v);
+                }
+            }
+        }
+    }
+    
+}
